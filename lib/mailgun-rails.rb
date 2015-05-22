c17bfb0e4716a917acb21c54fc60cf9604c92acd
Andri Setiawan <andri.setiawan@veritrans.co.id>
2015-05-22 17:33:52 +0700

    Add config for proxy & ssl_verify_peer.

--- lib/mailgun-rails.rb
+++ lib/mailgun-rails.rb
@@ -23,8 +23,15 @@ module Mailgun
       self.settings[:api_key]
     end
 
-    def deliver!(mail)
+    def http_proxy
+      self.settings[:http_proxy]
+    end
 
+    def ssl_verify_peer
+      self.settings[:ssl_verify_peer]
+    end
+
+    def deliver!(mail)
       body              = Curl::PostField.content("message", mail.encoded)
       body.remote_file  = "message"
       body.content_type = "application/octet-stream"
@@ -36,26 +43,24 @@ module Mailgun
         data << Curl::PostField.content("to", destination)
       end
 
-      curl                     = Curl::Easy.new("https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime")
-      curl.multipart_form_post = true
+      curl = Curl::Easy.new("https://api:#{self.api_key}@api.mailgun.net/v2/#{self.api_host}/messages.mime")
+
+      curl.proxy_url            = self.http_proxy if self.http_proxy
+      curl.ssl_verify_peer      = self.ssl_verify_peer
+      curl.multipart_form_post  = true
       curl.http_post(*data)
 
       if curl.response_code != 200
-
         begin
           error = ActiveSupport::JSON.decode(curl.body_str)["message"]
         rescue
           error = "Unknown Mailgun Error"
         end
-
         raise Mailgun::DeliveryError.new(error)
       end
-
     end
 
-
   end
-
 end
 
 ActionMailer::Base.add_delivery_method :mailgun, Mailgun::DeliveryMethod
